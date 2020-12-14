c0e2b3e91b20911fe3cf20f989c76fb9670690c3
Valdir Silva <valdir.ti@gmail.com>
2020-12-14 14:40:48 -0300

    chore: config eslint to import files .ts

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 
+import routes from './routes';
+
 const app = express();
 const PORT = 3333;
 
-app.get('/', (req, res) => {
-  return res.status(200).json({ message: 'Hello Typescript!' });
-});
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
