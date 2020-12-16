75c6a0f08b6065650ed18b849a79e4317e9df8b7
Valdir Silva <valdir.ti@gmail.com>
2020-12-16 16:46:42 -0300

    feat: create route to serve static file

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import 'reflect-metadata';
 import express from 'express';
 import routes from './routes';
+import uploadConfig from './config/upload';
+
 import './database';
 
 const PORT = 3333;
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.listen(PORT, () => {
