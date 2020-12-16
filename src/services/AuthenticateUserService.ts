da7b87b36c21202a29ef0b430189450226a05015
Valdir Silva <valdir.ti@gmail.com>
2020-12-16 15:05:48 -0300

    feat: use authConfig

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -2,6 +2,8 @@ import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 
+import authConfig from '../config/auth';
+
 import User from '../models/User';
 
 interface Request {
@@ -32,9 +34,11 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination');
     }
 
-    const token = sign({}, 'a819026cf59772e5998f2983acfa3f4a', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn,
     });
 
     return { user, token };
